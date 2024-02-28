9030c907cbd9c3cb513a4eab891539510027f71a
up1084066@ac.upatras.gr <up1084066@ac.upatras.gr>
2024-02-28 17:57:24 +0200

    first normal

--- lab1.py
+++ lab1.py
@@ -1,3 +1,38 @@
-print('hello world')
-print('hey there')
-print('hi beautiful')
+import requests
+
+def more(text):
+    count=0
+    for line in text.plit('\n'):
+        print(line)
+        count+=1
+        if count % 30 ==0:
+            reply=input('show more (y/n)? ')
+            if reply == 'n':
+                break
+
+url = input ('Enter a url: ')
+
+if not url.startswith('http://'):
+    url = 'http://' + url
+
+with requests.get(url) as response:
+
+    print(f"Website headers are {url} \n, {response.headers} \n\n")
+
+    server=response.headers.get('Server')
+
+    if server:
+        print (f"The server is {server}")
+    else:
+        print("no server found")
+
+    cookies = response.headers.get('Set-Cookie')
+
+    if cookies:
+        cookies=cookies.split(';')
+        for cookie in cookies:
+            print(f'The cookie is {cookie}')
+    
+    else:
+        print('No cookie found')
+
