ed76dbd95c80419c2c56757a1d1a80fea64132cc
up1084066@ac.upatras.gr <up1084066@ac.upatras.gr>
2024-02-28 20:05:09 +0200

    cookie name

--- lab1.py
+++ lab1.py
@@ -1,4 +1,5 @@
 import requests
+import re
 
 def more(text):
     count=0
@@ -15,24 +16,26 @@ url = input ('Enter a url: ')
 if not url.startswith('http://'):
     url = 'http://' + url
 
+print(url)
 with requests.get(url) as response:
 
     print(f"Website headers are {url} \n, {response.headers} \n\n")
 
-    server=response.headers.get('Server')
 
+    server=response.headers.get('Server')
     if server:
         print (f"The server is {server}")
     else:
         print("no server found")
 
-    cookies = response.headers.get('Set-Cookie')
-
-    if cookies:
-        cookies=cookies.split(';')
-        for cookie in cookies:
-            print(f'The cookie is {cookie}')
     
+    cookies = response.headers.get('Set-Cookie')
+    if cookies:
+        cookies=re.split('SameSite=[a-z]+, ',cookies)
+        for cookie in cookies:
+            cookie=cookie.split(';')
+            print(f"This cookie's name is {cookie[0]}")
+        
     else:
         print('No cookie found')
 
